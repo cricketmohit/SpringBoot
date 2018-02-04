a8469e03051e201f13b1e3929c2a9909d2682a24
Mohit <temp@gmail.com>
2018-02-05 00:48:41 +0100

    New Spring Boot Project

---
+++ demo/src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.model.Person;
+
+@Controller
+public class PersonController {
+
+	@RequestMapping("/person")
+	public String Person (Model model) {
+		Person p = new Person();
+		p.setFirstName("Mohit");
+		p.setLastName("Sharma");
+		p.setAge(33); 
+		model.addAttribute("person",p);
+		return "personview";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/")
+	String entry() {
+		return "My second Spring Boot App";
+	}
+}
